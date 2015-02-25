98c968ef31797d392cf0a944fd93fb9ae8e02f2f
emag <lanabe.lanabe@gmail.com>
2015-02-25 14:36:05 +0900

    Done chapter02

--- chapter02/hajiboot-layering/src/main/java/com/example/App.java
+++ chapter02/hajiboot-layering/src/main/java/com/example/App.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 /**
  * @author Yoshimasa Tanabe
@@ -23,7 +26,14 @@ public class App implements CommandLineRunner {
     Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
     System.out.println(created + " is created!");
 
-    customerRepository.findAll()
+    Pageable pageable = new PageRequest(0, 3);
+    Page<Customer> page = customerRepository.findAllOrderByName(pageable);
+    System.out.println("1 ページあたりのデータ数: " + page.getSize());
+    System.out.println("現在のページ: " + page.getNumber());
+    System.out.println("全ページ数: " + page.getTotalPages());
+    System.out.println("全データ数: " + page.getTotalElements());
+
+    page.getContent()
       .forEach(System.out::println);
   }
 
--- chapter02/hajiboot-layering/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS customers(
- id INT PRIMARY KEY AUTO_INCREMENT,
- first_name VARCHAR(30),
- last_name VARCHAR(30)
-);
